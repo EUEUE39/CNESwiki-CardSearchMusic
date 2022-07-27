fdccb4e3d46ad00f60b0145c253cf5ab830271cd
EU <1091499548@qq.com>
2022-07-27 18:37:07 +0800

    feat(App.vue PageTitle): 页面外边框确定

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
+  <page-title></page-title>
+  <MainPage></MainPage>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import MainPage from './components/MainPage'
+import PageTitle from "@/components/PageTitle";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    MainPage,
+    PageTitle,
   }
 }
 </script>
@@ -21,6 +29,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
+
+}
+body{
+  border:30px solid white;
+  background:rgb(228,232,233);
+  height: calc(100vh - 60px);
+  width: auto;
+  padding: 0;
+  margin:0;
 }
 </style>
---
+++ src/components/PageTitle.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="title" ref="ipt">
+  CardSearch!!
+<!--  A.SUBARU-->
+</div>
+</template>
+
+<script>
+export default {
+  name: "PageTitle",
+}
+</script>
+
+<style scoped>
+.title{
+  color:white;
+  font-weight: bolder;
+  font-size: 100px;
+  writing-mode:tb-rl;
+  position: absolute;
+  z-index:1;
+  margin:20px 0;
+  left:0;
+  bottom: 0;
+}
+</style>
