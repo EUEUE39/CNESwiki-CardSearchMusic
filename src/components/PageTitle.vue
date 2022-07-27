1ec228442e05e7f887b3d3a074db1fc71206a3c6
EU <1091499548@qq.com>
2022-07-27 19:16:30 +0800

    feat(PageTitle): 新增右下角黄色三角形

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,8 +1,12 @@
 <template>
-<div class="title" ref="ipt">
-  CardSearch!!
-<!--  A.SUBARU-->
-</div>
+  <div class="background">
+    <div class="title" ref="ipt">
+      CardSearch!!
+      <!--  A.SUBARU-->
+    </div>
+    <div class="subscript">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,15 +16,30 @@ export default {
 </script>
 
 <style scoped>
-.title{
-  color:white;
+/*.background{*/
+/*  display: flex;*/
+/*}*/
+
+.title {
+  color: white;
   font-weight: bolder;
   font-size: 100px;
-  writing-mode:tb-rl;
+  writing-mode: tb-rl;
   position: absolute;
-  z-index:1;
-  margin:20px 0;
-  left:0;
+  z-index: 1;
+  margin: 20px 0;
+  left: 0;
   bottom: 0;
+}
+
+.subscript{
+  position: absolute;
+  right: 30px;
+  bottom: 30px;
+  height: 0;
+  width: 0;
+  /*border:  solid 30px;*/
+  border-bottom: 20vh solid rgb(253,239,11);
+  border-left: 20vh solid transparent;
 }
 </style>
